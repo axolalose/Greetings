605ae281de13f8db6fb0a5dc2d8ab4f1b033840d
axolalose <axolalose@gmail.com>
2019-07-23 14:23:47 +0200

    new changes

--- greetings-test.js
+++ greetings-test.js
@@ -1,26 +1,39 @@
 describe("Greetings",function(){
-    it("Should return name and greet also in English ",function(){
+    it("Should return names and greet also in English ",function(){
         var greetMe = Greetings();
         greetMe.greet("axola");
         assert.equal(1 ,  greetMe.count()); 
     })
 
-    it("Should return name and greet also in Isixhosa", function(){
+    it("Should return names and greet also in Isixhosa", function(){
    var greetName = Greetings();
         greetName.greet("Sipho");
         assert.equal(1,  greetName.count());
     })
     
-    it("Should return name and greet also in Afrikaans", function(){
+    it("Should return names and greet also in Afrikaans", function(){
         var nameGreeted = Greetings();
         nameGreeted.greet("Nola");
         assert.equal(1,  nameGreeted.count());
          })
-    it("Should return name that is given to test in any language", function(){
+    it("Should return names that is given to test in any languages", function(){
         var nameTest = Greetings();
         nameTest.greet("Zikhona");
         assert.equal(1,  nameTest.count());
     })
-    
-    
-})
+//     it("When I click on any language without entering the name", function(){
+//         var nameTest = Greetings();
+//         nameTest.greet("name or lang not provided");
+//         assert.equal(1,  nameTest.count());
+//     })
+//     it("When I type in any name without clicking the language", function(){
+//         var nameTest = Greetings();
+//         nameTest.greet("name or lang not provided");
+//         assert.equal(1,  nameTest.count());
+//     })
+//     it("When I type in numbers should return unvalid", function(){
+//         var nameTest = Greetings();
+//         nameTest.greet("not valid");
+//         assert.equal(1,  nameTest.count());
+//     })
+ })
--- greetings.js
+++ greetings.js
@@ -2,15 +2,23 @@ function Greetings(loc) {
     var counter = 0;
     var result = ''
     var greetedName = loc || {}
-    //var name = "";
    
+    //var name = "";
+    
+
 
     function greet(name, lang) {
-        console.log(lang)
-        console.log(name)
+
+        var matches = name.match(/[a-zA-Z]+/g, "");
+        var sameName = name.toLowerCase()
+        var nameName = name.toUpperCase().charAt(0) + name.slice(1);
+        //console.log(lang)
+        //console.log(name)
         //counter++
-        if (greetedName[name] === undefined) {
-            greetedName[name] = 0;
+        if(matches !== null){
+    
+        if (greetedName[sameName] === undefined) {
+            greetedName[sameName] = 0;
         }
         console.log(greetedName)
 
@@ -19,32 +27,42 @@ function Greetings(loc) {
 
         } else {
             counter++
-
-        } if (lang === "Xhosa") {
-            return "Molo, " + name;
         }
+        // if {
+        //      matches = true;
 
-        if (lang === "English") {
-            return "Hello, " + name;
-        }
-        if (lang === "Afrikaans") {
-            return "Hallo, " + name;
-        }
+        // }
+    
+
+    if (lang === "Xhosa") {
+        return "Molo, " + nameName;
     }
 
-    function getName() {
-        var x = greetedName;
-        return x;
+    if (lang === "English") {
+        return "Hello, " + nameName;
     }
+    if (lang === "Afrikaans") {
+        return "Hallo, " + nameName;
+    }
+}
+else{
+    return" Not valid "
+}
+}
 
-    function count() {
-        var a = Object.keys(greetedName)
-        return a.length ;
-    }
+function getName() {
+    var x = greetedName;
+    return x;
+}
 
-    return {
-        greet,
-        getName,
-        count,
-    }
+function count() {
+    var a = Object.keys(greetedName)
+    return a.length;
+}
+
+return {
+    greet,
+    getName,
+    count,
+}
 }
